2d36a44d9e84685a28632d286954860945c0206a
mirandaiceron <mirandaiceron@gmail.com>
2025-10-18 16:08:30 +0100

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import RawPost
-
-# Register your models here.
-@admin.register(RawPost)
-class RawPostAdmin(admin.ModelAdmin):
-    list_display = ("id", "source", "created_at", "processed_at")
-    search_fields = ("caption", "source")
