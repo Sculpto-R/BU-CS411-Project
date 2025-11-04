fb6240a48eaece762796d0c769e6382b1d6e9c36
mirandaiceron <mirandaiceron@gmail.com>
2025-11-04 17:17:16 +0000

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField(blan = True)
+    date_start = models.CharField(max_length = 32, blank = True, null = True)
+    date_end = models.CharField(max_length = 32, blank = True, null = True)
+    location = models.CharField(max_length = 120, blank = True, null = True)
+    price_min = models.FloatField(blank = True, null = True)
+    price_max = models.FloatField(blank = True, null = True)
+    age_restriction = models.CharField(max_length = 16, blank = True, null = True)
+    ai_score = models.FloatField(default = 0.0)
+    created_at = models.DateTimeField(auto_now_add = True)
