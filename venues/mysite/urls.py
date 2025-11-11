c7109a51c9bdc179d7669b83007bea7ae9ea2081
Anya Gupta <anyagupta@MacBook-Air-3.local>
2025-11-11 09:51:31 +0000

    Add Django project setup and venues app

--- venues/mysite/urls.py
+++ venues/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for innit_project project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from venues.views import ping
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls", namespace="accounts")),
-    # ... other url
+    path('admin/', admin.site.urls),
+    path('venues/ping/', ping),
 ]
